77cd320602850edf305512baec593b420c9bc586
Robin Epple <>
2021-12-04 19:55:38 +0100

    Test Methods in their own lib

--- Centepede/TestCode/Startup.cpp
+++ Centepede/TestCode/Startup.cpp
@@ -1,34 +1,4 @@
-#include "../lib/console_lib.hpp"
-
-// ###############################
-// Framework Methods
-// ###############################
-// feel free to add more as you need
-
-void printTestName(std::string testName){
-    std::string dividerLine = "###############################";
-    println(dividerLine);
-    println(testName);
-    println(dividerLine);
-}
-
-void printResult(bool passed, std::string errorMessage){
-    AnsiExcapeCodes ansiExcapeCodes;
-    std::string result;
-    if(passed){
-        result = ansiExcapeCodes.foregroundGreen + "passed" + ansiExcapeCodes.foregroundDefault;
-    }else{ // not passed
-        result = ansiExcapeCodes.foregroundRed + "failed: " + errorMessage + ansiExcapeCodes.foregroundDefault;
-    }
-    println(result);
-}
-
-template<typename TObject>
-void assertEquals(TObject expected, TObject actual){
-    auto equal = expected == actual;
-    std::string errorMessage = "value: " + std::to_string(actual) + ", expected: " + std::to_string(expected);
-    printResult(equal, errorMessage);
-}
+#include "../lib/test_lib.hpp"
 
 // ###############################
 // Example Test Methods
